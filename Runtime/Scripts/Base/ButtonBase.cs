ec839f5738306273a2ea9c02b204609bce7e5f60
lshzang86 <lshzang86@naver.com>
2026-01-01 16:31:53 +0900

    ButonBase 구조 추가

---
+++ Runtime/Scripts/Base/ButtonBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public abstract class ButtonBase : UIBase
+{
+    [SerializeField] Button targetButton;
+
+    public void SetCustomButtonAction(UnityAction buttonAction) {
+        targetButton.onClick.RemoveAllListeners();
+        targetButton.onClick.AddListener(buttonAction);
+    }
+    protected abstract void ButtonAction();
+
+    protected override void Start() {
+        if (targetButton != null && !_isInit) {
+            SetCustomButtonAction(ButtonAction);
+            _isInit = true;
+        }
+    }
+}
