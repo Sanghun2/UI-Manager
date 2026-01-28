85892a00c31425dce3f3bf8cd251138644ce7967
lshzang86 <lshzang86@naver.com>
2026-01-28 14:56:46 +0900

    패키지 이름 충돌 방지용 BilliotGames namespace 추가

--- Runtime/Scripts/Base/ButtonBase.cs
+++ Runtime/Scripts/Base/ButtonBase.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public abstract class ButtonBase : UIBase
+namespace BilliotGames
 {
-    [SerializeField] Button targetButton;
+    public abstract class ButtonBase : UIBase
+    {
+        [SerializeField] Button targetButton;
 
-    public void SetCustomButtonAction(UnityAction buttonAction) {
-        targetButton.onClick.RemoveAllListeners();
-        targetButton.onClick.AddListener(buttonAction);
-    }
-    protected abstract void ButtonAction();
+        public void SetCustomButtonAction(UnityAction buttonAction) {
+            targetButton.onClick.RemoveAllListeners();
+            targetButton.onClick.AddListener(buttonAction);
+        }
+        protected abstract void ButtonAction();
 
-    protected override void Start() {
-        if (targetButton != null && !_isInit) {
-            SetCustomButtonAction(ButtonAction);
-            _isInit = true;
+        protected override void Start() {
+            if (targetButton != null && !_isInit) {
+                SetCustomButtonAction(ButtonAction);
+                _isInit = true;
+            }
         }
     }
 }
+
--- Runtime/Scripts/Base/UIBase.cs
+++ Runtime/Scripts/Base/UIBase.cs
@@ -1,29 +1,32 @@
 ﻿using UnityEngine;
 
-public abstract class UIBase : MonoBehaviour
+namespace BilliotGames
 {
-    public virtual bool IsOpened => gameObject.activeSelf;
-    public bool IsInit => _isInit;
-    protected bool _isInit;
+    public abstract class UIBase : MonoBehaviour
+    {
+        public virtual bool IsOpened => gameObject.activeSelf;
+        public bool IsInit => _isInit;
+        protected bool _isInit;
 
-    public virtual void InitUI() { }
+        public virtual void InitUI() { }
 
-    public virtual void OpenUI() {
-        gameObject.SetActive(true);
-        OnOpen();
-    }
-    public virtual void CloseUI() {
-        gameObject.SetActive(false);
-        OnClose();
-    }
+        public virtual void OpenUI() {
+            gameObject.SetActive(true);
+            OnOpen();
+        }
+        public virtual void CloseUI() {
+            gameObject.SetActive(false);
+            OnClose();
+        }
 
-    protected virtual void OnOpen() { }
-    protected virtual void OnClose() { }
+        protected virtual void OnOpen() { }
+        protected virtual void OnClose() { }
 
-    protected virtual void Start() {
-        if (!_isInit) {
-            InitUI();
-            _isInit = true;
+        protected virtual void Start() {
+            if (!_isInit) {
+                InitUI();
+                _isInit = true;
+            }
         }
     }
 }
