2d02c41eda836f51f43464f8e29a145826112899
lshzang86 <lshzang86@naver.com>
2026-02-04 14:14:21 +0900

    TextBase 추가

---
+++ Runtime/Scripts/Base/TextBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace BilliotGames
+{
+    public class TextBase : UIBase
+    {
+        [SerializeField] TextMeshProUGUI targetText;
+
+        public virtual void SetText(string text) {
+            targetText.text = text; 
+        }
+
+        protected virtual void Reset() {
+            if (targetText == null) {
+                targetText = GetComponentInChildren<TextMeshProUGUI>();
+            }
+        }
+    }
+}
