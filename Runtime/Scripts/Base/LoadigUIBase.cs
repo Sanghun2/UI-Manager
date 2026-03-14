0c877637453a6b72d18105d20ff4208c58074a3b
lshzang86 <lshzang86@naver.com>
2026-03-14 16:07:24 +0900

    Loading UI 추가

---
+++ Runtime/Scripts/Base/LoadigUIBase.cs
@@ -0,0 +1,9 @@
+﻿namespace BilliotGames
+{
+    public abstract class LoadigUIBase : UIBase
+    {
+        public abstract void StartLoading();
+        public abstract void UpdateLoading(float currentValue, float totalValue);
+        public abstract void StopLoading();
+    }
+}
